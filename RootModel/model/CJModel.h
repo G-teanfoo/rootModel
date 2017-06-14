ac13375db8bf8b5c596b9c6f4c2029e92919d29b
chjsun <sunchj@51laila.com>
2017-06-14 15:13:20 +0800

    完善模型

--- RootModel/model/CJModel.h
+++ RootModel/model/CJModel.h
@@ -1,16 +1,19 @@
 //
-//  JRModel.h
-//  parking
+//  CJModel.h
 //
 //  Created by chjsun on 2016/12/9.
 //  Copyright © 2016年 chjsun. All rights reserved.
-//
+//  基础模型，该模型不是用于数据加载时使用，而是用于需要全局通用的数据的存储，
+//  例如用户信息，用户状态，用户当前经纬度，屏幕中心点坐标等等在其他界面可能用到的数据，在此model的
+//  子类中保存，
+//  思想：单例
+//  用法：继承此model，将属性声明在.h文件中即可。.m可以不写任何代码。也可以写
 
 #import <Foundation/Foundation.h>
 
-@interface CJModel : NSObject
+@interface JRModel : NSObject
 /**
- *  创建共享唯一model
+ *  创建共享唯一模型返回model
  */
 + (id)shareModel;
 
@@ -18,4 +21,15 @@
  *  通过kvc快速给属性赋值
  */
 - (void)setValueForDict:(NSDictionary *)dict;
+
+/**
+ *  将所有属性值置为nil
+ */
+- (void)clearModelInfo;
+
+/**
+ *  格式化对象，打印出来
+ */
+- (NSDictionary *)formatModel;
+
 @end
--- RootModel/model/CJModel.m
+++ RootModel/model/CJModel.m
@@ -1,44 +1,48 @@
 //
-//  CJModel.m
+//  JRModel.m
 //  parking
 //
 //  Created by chjsun on 2016/12/9.
 //  Copyright © 2016年 chjsun. All rights reserved.
 //
 
-#import "CJModel.h"
-
+#import "JRModel.h"
 static NSMutableDictionary *storagePool;
 
-static CJModel *model;
+static JRModel *model;
 
-@implementation CJModel
+@implementation JRModel
 
 + (id)shareModel{
 
-    @synchronized(self){
+    NSString *modelClass = NSStringFromClass([self class]);
+    if (storagePool != nil && [storagePool objectForKey:modelClass]) {//首先判断缓存池或缓存池是否存在
+        model = storagePool[modelClass];
 
-        if (storagePool == nil) {//首先判断容器是否存在
-            storagePool = [[NSMutableDictionary alloc] init];//不存在就先创建
+    }else{
+        @synchronized(self){
+            if (storagePool == nil) {//首先判断缓存池是否存在
+                storagePool = [[NSMutableDictionary alloc] init];//不存在就先创建
+            }
+            if ([storagePool objectForKey:modelClass]) {
+                return storagePool[modelClass];
+            }else{
+                model = [[self alloc] init];
+            }
         }
-        NSString *modelClass = NSStringFromClass([self class]);
-        if ([storagePool objectForKey:modelClass]) {
-            return storagePool[modelClass];
-        }else{
-            model = [[self alloc] init];
-        }
+
     }
     return model;
 }
 
-    //重写alloc方法，保证在使用alloc、new 去创建对象时，不产生新的对象
+//重写alloc方法，保证在使用alloc、new 去创建对象时，不产生新的对象
 + (id)allocWithZone:(NSZone *)zone{
     NSString *modelClass = NSStringFromClass([self class]);
-//
+    //
     if (storagePool == nil) {
         storagePool = [[NSMutableDictionary alloc] init];;
     }
-//
+    //
     if ([storagePool objectForKey:modelClass]) {
         return storagePool[modelClass];
     }else{
@@ -48,32 +52,80 @@ static CJModel *model;
     return model;
 }
 
-// 允许copy,程序员在使用时肯定知道copy的含义，不知道请去查，留这个接口是为了某些可能需要的特殊的业务
+//    //允许copy,程序员在使用时肯定知道copy的含义，不知道请去查，留这个接口是为了某些可能需要的特殊的业务
 //- (id)copyWithZone:(NSZone *)zone{
 //    return model;
 //}
 //
-// 允许mutableCopy,程序员在使用时肯定知道mutable的含义，不知道请去查，留这个接口是为了某些可能需要的特殊的业务
+//    //允许copy,程序员在使用时肯定知道mutable的含义，不知道请去查，留这个接口是为了某些可能需要的特殊的业务
 //- (id)mutableCopyWithZone:(NSZone *)zone{
 //    return model;
 //}
 
-    // KVC 快速设置值
+// 快速设置值
 - (void)setValueForDict:(NSDictionary *)dict{
     [self setValuesForKeysWithDictionary:dict];
 }
 
+//
+- (void)clearModelInfo{
+    // 记录属性的个数
+    unsigned int propertyCount = 0;
+    objc_property_t *propertys = class_copyPropertyList([self class], &propertyCount);
 
-    // 设置不存在 key 的值
-- (void)setValue:(id)value forUndefinedKey:(NSString *)key {
+    // 把属性放到数组中
+    for (int i = 0; i < propertyCount; i ++) {
+        // 取出每一个属性
+        objc_property_t property = propertys[i];
+        const char *propertyName = property_getName(property);
+        // 将所有属性值设置为nil
 
-    NSLog(@"key 值 %@ 不存在，无法设置值 ！", key);
+        //  number类型就放@(0)
+        if ([[self valueForKey:[[NSString alloc] initWithUTF8String:propertyName]] isKindOfClass:[NSNumber class]]) {
+            [self setValue:@(0) forKey:[[NSString alloc] initWithUTF8String:propertyName]];
+            return;
+        }
+
+        [self setValue:nil forKey:[[NSString alloc] initWithUTF8String:propertyName]];
+    }
 }
 
-    // 获取不存在的 key 的值
+// 格式化对象
+- (NSDictionary *)formatModel{
+
+    // 记录属性的个数
+    unsigned int propertyCount = 0;
+    objc_property_t *propertys = class_copyPropertyList([self class], &propertyCount);
+
+    NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+    // 把属性放到数组中
+    for (int i = 0; i < propertyCount; i ++) {
+        // 取出每一个属性
+        objc_property_t property = propertys[i];
+        const char *propertyName = property_getName(property);
+        // 将所有属性值赋值给字典
+        id value = [self valueForKey:[[NSString alloc] initWithUTF8String:propertyName]];
+        if (value == nil) {
+            value = @"nil";
+        }
+        [dict setObject:value forKey:[[NSString alloc] initWithUTF8String:propertyName]];
+    }
+
+    return dict;
+
+}
+
+
+// 设置不存在 key 的值
+- (void)setValue:(id)value forUndefinedKey:(NSString *)key {
+
+    NSLog(@"%@ key 值 %@ 未定义，无法设置值 ！", [self class], key);
+}
+
+// 获取不存在的 key 的值
 - (id)valueForUndefinedKey:(NSString *)key {
 
-    NSLog(@"key 值 %@ 不存在，无法获取值 ！", key);
+    NSLog(@"%@ key 值 %@ 不存在，无法获取值 ！", [self class], key);
     
     return nil;
 }
